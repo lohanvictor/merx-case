8e40ce1912703a7a474a639ad90a2f1f7642a9fa
lohanvictor <lohan.victor@hotmail.com>
2025-08-21 19:24:52 -0400

    feat: add header in dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,12 @@
+import Header from "@/_components/header";
+
 export default function DashboardPage() {
-    return <div>Dashboard</div>;
+  return (
+    <main className="flex flex-col items-center w-full h-full bg-white">
+      <Header
+        title="Dashboard Agrícola"
+        subtitle="Monitoramento de Commodities em Tempo Real"
+      />
+    </main>
+  );
 }
