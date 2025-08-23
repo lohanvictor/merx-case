d19689a60aa0f9e0a29e0522ef8bc81cf6a00bfe
lohanvictor <lohan.victor@hotmail.com>
2025-08-22 20:01:59 -0400

    feat: create amount card for dashboard

---
+++ src/_components/amountCard/index.tsx
@@ -0,0 +1,51 @@
+import { LucideProps } from "lucide-react";
+import { ForwardRefExoticComponent, RefAttributes } from "react";
+
+type Props = {
+  label: string;
+  icon: {
+    element: ForwardRefExoticComponent<
+      Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
+    >;
+    className: string;
+  };
+  containerClassname: string;
+  amount: string;
+  etf?: boolean;
+  date: string;
+  percentage: number;
+};
+
+export default function AmountCard(props: Props) {
+  return (
+    <div
+      className={`border-2 rounded-lg p-6 shadow-xs ${props.containerClassname} flex flex-col w-full gap-1`}
+    >
+      <div className="flex justify-between">
+        <span className="text-base font-bold text-gray-900">{props.label}</span>
+        <props.icon.element size={28} className={props.icon.className} />
+      </div>
+      <div className="">
+        <span className="text-2xl font-bold text-gray-900">{props.amount}</span>
+        {props.etf && (
+          <span className="text-gray-600 font-semibold text-xs uppercase tracking-wider">
+            /etf
+          </span>
+        )}
+      </div>
+      <div className="flex justify-between items-center">
+        <span className="text-gray-600 text-xs font-medium">{props.date}</span>
+
+        <span
+          className={`inline-flex items-center justify-center px-2 py-1 text-xs font-medium ${
+            props.percentage > 0
+              ? "text-green-800 bg-green-100"
+              : "text-red-800 bg-red-100"
+          } rounded-full`}
+        >
+          {props.percentage}%
+        </span>
+      </div>
+    </div>
+  );
+}
