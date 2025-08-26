2a2672aa93e12064415960f8b8ea93f1b4529d9c
lohanvictor <lohan.victor@hotmail.com>
2025-08-26 10:07:56 -0400

    feat: create logout button

---
+++ src/_components/logoutButton/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { LogOut } from "lucide-react";
+import { useState } from "react";
+import OptionalModal from "../optionalModal";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  classname?: string;
+};
+
+export default function LogoutButton({ classname }: Props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const route = useRouter();
+
+  const handleLogout = () => {
+    Cookies.remove("token");
+    route.push("/login");
+  };
+
+  return (
+    <>
+      <button
+        className={`px-4 py-2    flex items-center gap-2 border rounded border-gray-300 text-gray-600 bg-white hover:bg-gray-100 hover:cursor-pointer ${classname}`}
+        onClick={() => setIsModalOpen(true)}
+      >
+        <LogOut size={20} /> <span className="font-medium text-sm">Logout</span>
+      </button>
+      {isModalOpen && (
+        <OptionalModal
+          cancelProps={{
+            text: "Cancelar",
+            onClick: () => setIsModalOpen(false),
+          }}
+          confirmProps={{
+            text: "Sair",
+            onClick: handleLogout,
+          }}
+          text="Tem certeza de que deseja sair?"
+        />
+      )}
+    </>
+  );
+}
