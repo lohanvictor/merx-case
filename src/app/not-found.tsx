ae57ba90c4afd5b3bde3e44a5f056b5ec247072a
lohanvictor <lohan.victor@hotmail.com>
2025-08-25 20:09:11 -0400

    feat: add general not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,12 @@
 export default function NotFoundPage() {
-  return <div>Not Found</div>;
+  return (
+    <section className="w-full p-4">
+      <div className="flex flex-col border rounded-lg p-4">
+        <h2 className="text-7xl">
+          A página acessada <br />{" "}
+          <span className="text-5xl">não está disponível no momento.</span>
+        </h2>
+      </div>
+    </section>
+  );
 }
