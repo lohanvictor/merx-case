226bdd3f7146411eba981fecfb136d1da6ae7b8c
lohanvictor <lohan.victor@hotmail.com>
2025-08-25 21:51:16 -0400

    feat: add middleware for guard route

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const PUBLIC_ROUTES = ["/login", "/"];
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const isPublicRoute = PUBLIC_ROUTES.includes(path);
+  const token = request.cookies.get("token")?.value;
+
+  if (isPublicRoute && token) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (!isPublicRoute && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
