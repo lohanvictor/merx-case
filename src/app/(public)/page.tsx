0fee9a6ffea7b3baf408241baf795b26084076db
lohanvictor <lohan.victor@hotmail.com>
2025-10-20 12:37:39 -0400

    feat: add description of case in home page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -4,10 +4,45 @@ export default function Home() {
       <div className="p-4 flex flex-col">
         <h1 className="text-4xl font-bold">Case Merx Tech</h1>
         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
-          delectus vero soluta illum minus! Optio, cum repellendus! Quia rerum
-          accusamus maxime, neque necessitatibus voluptas, nostrum voluptatum
-          quasi possimus ut numquam!
+          Este projeto é a implementação de um case para um processo seletivo
+          para Desenvolvedor Front-End JR na Merx Tech.
+        </p>
+
+        <p>
+          O principal objetivo é desenvolver um dashboard web que replique o
+          layout do print anexado, exibindo preços de commodities agrícolas
+          (como milho, trigo e soja) integrados a API da{" "}
+          <a
+            href="https://commodities-api.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline text-blue-500"
+          >
+            CommoditiesAPI
+          </a>
+          .
+        </p>
+        <p>O desafio avalia sua capacidade de:</p>
+        <ul className="list-disc pl-5">
+          <li>Consumir APIs</li>
+          <li>Criar interfaces responsivas</li>
+          <li>Gerenciar dados</li>
+          <li>Escrever código de qualidade</li>
+          <li>Responsividade mobile</li>
+          <li>Funcionalidade de login (opcional)</li>
+        </ul>
+
+        <p>
+          O repositório para o projeto pode ser encontrado no seguinte{" "}
+          <a
+            href="https://github.com/lohanvictor/merx-case"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline text-blue-500"
+          >
+            link
+          </a>
+          .
         </p>
       </div>
     </div>
