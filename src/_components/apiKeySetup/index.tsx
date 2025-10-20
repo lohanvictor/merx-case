56c09f892c273ec3ee9e7768088cd95a0b66012d
lohanvictor <lohan.victor@hotmail.com>
2025-10-20 12:27:25 -0400

    feat: add DropdownTip in APi Key Setup component

--- src/_components/apiKeySetup/index.tsx
+++ src/_components/apiKeySetup/index.tsx
@@ -5,6 +5,7 @@ import Cookies from "js-cookie";
 import { useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import { API_KEY_COOKIE_NAME } from "@/_constants";
+import { DropdownTip } from "../dropdownTip";
 
 type Props = {
   apiKey: string;
@@ -24,7 +25,7 @@ export default function ApiKeySetup(props: Props) {
   }
 
   function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       handleApiKey();
     }
   }
@@ -36,39 +37,56 @@ export default function ApiKeySetup(props: Props) {
 
   if (!props.apiKey || isEdit) {
     return (
-      <section className="w-full border bg-blue-50 border-blue-300 p-8 py-6 rounded-lg flex flex-col sm:flex-row sm:items-center gap-3 text-blue-700">
-        <Key className="sm:block hidden" />
-        <span className="text-lg font-medium text-center sm:text-left">
-          {isEdit ? "Edite sua API Key" : "API Key não está definida"}
-        </span>
-        <form className="sm:ml-auto gap-2 flex flex-col sm:flex-row sm:items-center">
-          <input
-            type="text"
-            name="apiKey"
-            placeholder="Insira sua API Key"
-            className="p-2 border rounded-md border-gray-300 bg-white text-gray-600"
-            defaultValue={props.apiKey}
-            onChange={(e) => (apiKeyInputRef.current = e.target.value)}
-            onKeyDown={handleKeyDown}
-          />
-          <button
-            type="button"
-            onClick={handleApiKey}
-            className="text-base font-medium bg-white hover:cursor-pointer text-gray-900 border rounded-md border-gray-300 p-2"
-          >
-            Salvar
-          </button>
-          {isEdit && (
+      <div className="flex flex-col gap-2">
+        <section className="w-full border bg-blue-50 border-blue-300 p-8 py-6 rounded-lg flex flex-col sm:flex-row sm:items-center gap-3 text-blue-700">
+          <Key className="sm:block hidden" />
+          <span className="text-lg font-medium text-center sm:text-left">
+            {isEdit ? "Edite sua API Key" : "API Key não está definida"}
+          </span>
+          <form className="sm:ml-auto gap-2 flex flex-col sm:flex-row sm:items-center">
+            <input
+              type="text"
+              name="apiKey"
+              placeholder="Insira sua API Key"
+              className="p-2 border rounded-md border-gray-300 bg-white text-gray-600"
+              defaultValue={props.apiKey}
+              onChange={(e) => (apiKeyInputRef.current = e.target.value)}
+              onKeyDown={handleKeyDown}
+            />
             <button
               type="button"
-              onClick={() => setIsEdit(false)}
+              onClick={handleApiKey}
               className="text-base font-medium bg-white hover:cursor-pointer text-gray-900 border rounded-md border-gray-300 p-2"
             >
-              Voltar
+              Salvar
             </button>
-          )}
-        </form>
-      </section>
+            {isEdit && (
+              <button
+                type="button"
+                onClick={() => setIsEdit(false)}
+                className="text-base font-medium bg-white hover:cursor-pointer text-gray-900 border rounded-md border-gray-300 p-2"
+              >
+                Voltar
+              </button>
+            )}
+          </form>
+        </section>
+        <div className="flex justify-end">
+          <DropdownTip
+            position="right"
+            title="Dica para API Key"
+            customClasses="w-80 sm:w-[500px]"
+          >
+            <div className="p-4 bg-gray-100 border border-gray-300 rounded">
+              <span className="font-semibold mb-2">
+                Caso não possua API Key da CommodityAPI, inserir API Key de
+                dados mockados:
+              </span> {' '}
+              <span>MOCK_API_KEY</span>
+            </div>
+          </DropdownTip>
+        </div>
+      </div>
     );
   }
 
